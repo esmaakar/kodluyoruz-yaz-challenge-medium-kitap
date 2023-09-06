46e12959dbd310c638193b689fecad690658de52
esmaakar <esmaakar171@gmail.com>
2023-09-06 20:56:48 +0300

    Create kitap.c

---
+++ kitap.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[]) {
+	int x=36,ay=12,kitapsayisi;
+	kitapsayisi=x/ay;
+	printf("1 ayda okuyacagi kitap sayisi %d'tur.\n1 yilda okuyacagi kitap sayisi %d'dir",kitapsayisi,x);
+	
+	return 0;
+}
